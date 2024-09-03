d6504cda568c3c5bbae7c427e70d045ec2d151cc
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-09-03 14:24:02 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center space-x-4 my-4"></div>
+      <h1>Template React/vite Tailwind</h1>
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
