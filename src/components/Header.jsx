30aa4604962b343d623848efe88141adfa1fa8b8
Joshua <joshua.jaup@hotmail.com>
2024-09-04 14:11:57 +0200

    react icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-gray-900 sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b-4- p-8 text-white">
-      <h1>Logo</h1>
+      <h1><span className="text-teal-400">Fake Store</span></h1>
       <NavLink to="/HomePage.jsx">Home</NavLink>
       <NavLink to="/CartPage.jsx">Cart</NavLink>
     </header>
