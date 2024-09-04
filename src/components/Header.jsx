df234df78b620b9019aaf2a14fd48143afa78691
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-09-04 14:11:34 +0200

    fixed NavLinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
   return (
     <header className="bg-gray-900 sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b-4- p-8 text-white">
       <h1>Logo</h1>
-      <NavLink to="/HomePage.jsx">Home</NavLink>
-      <NavLink to="/CartPage.jsx">Cart</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
     </header>
-  )
+  );
 };
 
 export default Header;
