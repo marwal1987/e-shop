58d9193d47a037c82f02afdfd125c87398d5d829
Joshua <joshua.jaup@hotmail.com>
2024-09-04 13:54:23 +0200

    updated header footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,31 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>Jag är en Footer</div>
+    <footer className="bg-gray-900 text-white fixed bottom-0 w-full" >
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1
+          className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold
+         md:w-2/5"
+        >
+          <span className="text-teal-400">Fake Store AB</span>
+        </h1>
+        <div>
+            <div className="flex justify-between p-4">
+              <a>Instagram</a>
+              <a>X</a>
+              <a>Facebook</a>
+            </div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-12">
+        <span>© 2024 Grupp 5. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 // Example: Header.js
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <header>Jag är en header</header>;
+  return (
+    <header className="bg-gray-900 sticky top-0 z-[20] mx-auto flex w-full items-center justify-between border-b-4- p-8 text-white">
+      <h1>Logo</h1>
+      <NavLink to="/HomePage.jsx">Home</NavLink>
+      <NavLink to="/CartPage.jsx">Cart</NavLink>
+    </header>
+  )
 };
 
 export default Header;
