2cca0d33e64b14a675223ef6506f5694e4fe2ea4
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-09-10 11:30:24 +0200

    changed css in productCard for mote concise placement of button and Link

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,18 +8,18 @@ import { Link } from "react-router-dom";
 // Link används för att länka till en enskild produktsida baserat på produktens id (t.ex. /product/1).
 const ProductCard = ({ product, addToCart }) => {
   return (
-    <div className="w-full pb-4 gap-6 rounded-md shadow-md flex flex-col items-center justify-center ">
+    <div className="w-full p-6 gap-6 rounded-md shadow-md flex flex-col items-center justify-between ">
       <Link
         to={`/product/${product.id}`}
-        className="w-full flex flex-col items-center justify-center"
+        className="w-full flex flex-col "
       >
         <img src={product.image} alt={product.title} className="mb-6 h-96 rounded-t-md" />
         <h2 className="text-lg font-bold text-center mb-6">{product.title}</h2>
-      </Link>
         <p className="text-gray-600 text-center mb-6">${product.price}</p>
+      </Link>
       <button
         onClick={() => addToCart(product)}
-        className=" bg-blue-500 text-white p-2 m-2 rounded"
+        className=" bg-blue-500 text-white p-2 rounded"
       >
         Add to Cart
       </button>
